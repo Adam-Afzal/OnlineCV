b3e11b4a0eb6d24ffc93d95de93d09136cf2979a
QAJamesClive <Jamesclive@academytrainee.com>
2018-08-09 17:26:54 +0100

    test

---
+++ Garage.js
@@ -0,0 +1,25 @@
+vehicles = [];
+
+function addVehicle(){
+
+    var name = document.getElementById("vehName").value;
+    var size = document.getElementById("vehSize").value;
+
+
+        var car={};
+        car.name = name;
+        car.size = size;
+
+
+  vehicles.push(car);
+    console.log(car.name);
+
+}
+function calcBill(){
+
+
+
+
+
+
+}
